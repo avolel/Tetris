815835620edc1934ceb3cc5b9941944c51826442
Andre Volel <avolel@gmail.com>
2022-10-22 21:23:08 -0400

    Implemented a better scoring system

--- Tetris/GameState.cs
+++ Tetris/GameState.cs
@@ -28,7 +28,7 @@
         public bool GameOver { get; private set; }
         public bool GamePaused { get; private set; }
         public int Score { get; private set; }
-        public int Level { get; private set; } = 0;
+        public int Level { get; private set; } = 1;
         public int Delay { get; private set; } = 1200;
         public Block HeldBlock { get; private set; }
         public bool CanHold { get; private set; }
@@ -112,149 +112,35 @@
 
         }
 
+        private void ScoreAndLevelUp(int point, int pointMultiplier)
+        {
+            if (Delay <= 0)
+                Delay = 1;
+            else
+                Delay -= 1;
+            Score = Score + (point * pointMultiplier);
+            Level += 1;
+        }
+
+        private void Scoring(int point, int pointMultiplier)
+        {
+            if (Delay <= 0)
+                Delay = 1;
+            else
+                Delay -= 1;
+            Score = Score + (point * pointMultiplier);
+        }
+
         private void CalculateScore(int numRowsCleared)
         {
-            switch (Level)
-            {
-                case 0:
-                    if (numRowsCleared == 1)
-                        Score = Score + 40;
-                    else if (numRowsCleared == 2)
-                    {
-                        Score = Score + 100;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    else if (numRowsCleared == 3)
-                    {
-                        Score = Score + 300;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    else if (numRowsCleared >= 4)
-                    {
-                        Score = Score + 1200;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    break;
-                case 1:
-                    if (numRowsCleared == 1)
-                        Score = Score + 80;
-                    else if (numRowsCleared == 2)
-                    {
-                        Score = Score + 200;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    else if (numRowsCleared == 3)
-                    {
-                        Score = Score + 600;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    else if (numRowsCleared >= 4)
-                    {
-                        Score = Score + 2400;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    break;
-                case 2:
-                    if (numRowsCleared == 1)
-                        Score = Score + 120;
-                    else if (numRowsCleared == 2)
-                    {
-                        Score = Score + 300;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    else if (numRowsCleared == 3)
-                    {
-                        Score = Score + 900;
-                        Level += 1;
-                        Delay -= 1;
-                    }
-                    else if (numRowsCleared >= 4)
-                    {
-                        Score = Score + 3600;
-                        Level += 1;
-                        Delay -=1;
-                    }
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                    if (numRowsCleared == 1)
-                        Score = Score + 120;
-                    else if (numRowsCleared == 2)
-                    {
-                        Score = Score + 300;
-                        Level += 1;
-                        Delay -= 4;
-                    }
-                    else if (numRowsCleared == 3)
-                    {
-                        Score = Score + 900;
-                        Level += 1;
-                        Delay -= 4;
-                    }
-                    else if (numRowsCleared >= 4)
-                    {
-                        Score = Score + 3600;
-                        Level += 1;
-                        Delay -= 4;
-                    }
-                    break;
-                case 9:
-                    if (numRowsCleared == 1)
-                        Score = Score + 400;
-                    else if (numRowsCleared == 2)
-                    {
-                        Score = Score + 1000;
-                        Level += 1;
-                        Delay -= 10;
-                    }
-                    else if (numRowsCleared == 3)
-                    {
-                        Score = Score + 3000;
-                        Level += 1;
-                        Delay -= 10;
-                    }
-                    else if (numRowsCleared >= 4)
-                    {
-                        Score = Score + 12000;
-                        Level += 1;
-                        Delay -= 10;
-                    }
-                    break;
-                default:
-                    if (numRowsCleared == 1)
-                    {
-                        Score = Score + 40 * (Level + 1);
-                        Delay = Delay - 40;
-                    }
-                    else if (numRowsCleared == 2)
-                    {
-                        Score = Score + 100 * (Level + 1);
-                        Delay = Delay - 100;
-                    }
-                    else if (numRowsCleared == 3)
-                    {
-                        Score = Score + 300 * (Level + 1);
-                        Delay = Delay - 300;
-                    }
-                    else if (numRowsCleared >= 4)
-                    {
-                        Score = Score + 1200 * (Level + 1);
-                        Delay = Delay - 1200;
-                    }
-                    break;
-            }
-
+            if (numRowsCleared == 1)
+                Scoring(1, 5);
+            else if (numRowsCleared == 2)
+                ScoreAndLevelUp(5, 6);
+            else if (numRowsCleared == 3)
+                ScoreAndLevelUp(20, 15);
+            else if (numRowsCleared >= 4)
+                ScoreAndLevelUp(80, 100);
         }
 
         private int TileDropDistance(Position p)
